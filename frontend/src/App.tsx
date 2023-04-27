307c75f6cc402dcecfd9278c9764f6e38a161caa
Tahmid Rahman <tr920@ic.ac.uk>
2023-04-27 18:05:47 +0100

    feat: formatting settings configured

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,9 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { useQuery } from 'react-query';
-import Button from 'react-bootstrap/Button';
-import Carousel from 'react-bootstrap/Carousel'
-
-
-
+import React from "react";
+import { useState, useEffect } from "react";
+import { useQuery } from "react-query";
+import Button from "react-bootstrap/Button";
+import Carousel from "react-bootstrap/Carousel";
 
 export default function App() {
-
-  return (
-    <>
-    </>
-  );
+  return <></>;
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import reportWebVitals from './reportWebVitals';
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import reportWebVitals from "./reportWebVitals";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: Infinity,
-      cacheTime: Infinity
-    }
-  }
-})
+      cacheTime: Infinity,
+    },
+  },
+});
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
