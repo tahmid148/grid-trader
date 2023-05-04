5a4a9325eee994572194ca253f67673b4107d4bc
Tahmid Rahman <tr920@ic.ac.uk>
2023-05-05 00:41:49 +0100

    feat: subscription to Ethusd created

--- backend-nodejs/scripts/gridbot.js
+++ backend-nodejs/scripts/gridbot.js
@@ -1,28 +1,33 @@
 const API_KEY= "" // PUT YOUR KEY HERE
-const SECRET_KEY= "" 
+const SECRET_KEY= ""
 
 const url = "wss://stream.data.alpaca.markets/v1beta3/crypto/us"
 const webSocket = new WebSocket(url)
 
-console.log(webSocket)
-
 webSocket.onmessage = (event) => {
     const data = JSON.parse(event.data)
+    const message = data[0]['msg']
     console.log(data)
 
-    if (data[0]['msg'] === 'connected') {
+    if (message === 'connected') {
         console.log("Requesting Authentication")
         sendAuthentication()
-    } else if (data[0]['msg'] === 'authenticated') {
+    } else if (message === 'authenticated') {
         console.log("Authentication Successful!")
-    } else {
+        sendSubscription()
+    } else if (message === 'auth failed') {
         console.log("Authentication Failed")
+    } else if (message === 'auth timeout') {
+        console.log("Authentication Timed Out")
     }
 }
 
 const sendAuthentication = () => {
-    const authMessage = {"action": "auth", "key": process.env.API_KEY, "secret": process.env.SECRET_KEY}
-
-    console.log(`This is the message: ${authMessage}`)
+    const authMessage = {"action": "auth", "key": API_KEY, "secret": SECRET_KEY}
     webSocket.send(JSON.stringify(authMessage))
+}
+
+const sendSubscription = () => {
+    const subscribeMessage = {"action":"subscribe","trades":["ETH/USD"],"quotes":["ETH/USD"],"bars":["ETH/USD"]}
+    webSocket.send(JSON.stringify(subscribeMessage))
 }
