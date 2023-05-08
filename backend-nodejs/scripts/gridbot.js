432399b5555db4604cd2341c03193b02aebecb89
Tahmid Rahman <tr920@ic.ac.uk>
2023-05-08 23:07:16 +0100

    feat: the shown graph now shows last 2 hours of activity

--- backend-nodejs/scripts/gridbot.js
+++ backend-nodejs/scripts/gridbot.js
@@ -36,25 +36,36 @@ var chart = LightweightCharts.createChart(document.getElementById('chart'), {
 
 var candleSeries = chart.addCandlestickSeries();
 
-var data = [
-	{ time: '2018-10-19', open: 54.62, high: 55.50, low: 54.52, close: 54.90 },
-	{ time: '2018-10-22', open: 55.08, high: 55.27, low: 54.61, close: 54.98 },
-	{ time: '2018-10-23', open: 56.09, high: 57.47, low: 56.09, close: 57.21 },
-	{ time: '2018-10-24', open: 57.00, high: 58.44, low: 56.41, close: 57.42 },
-	{ time: '2018-10-25', open: 57.46, high: 57.63, low: 56.17, close: 56.43 },
-	{ time: '2018-10-26', open: 56.26, high: 56.62, low: 55.19, close: 55.51 },
-	{ time: '2018-10-29', open: 55.81, high: 57.15, low: 55.72, close: 56.48 },
-	{ time: '2018-10-30', open: 56.92, high: 58.80, low: 56.92, close: 58.18 },
-	{ time: '2018-10-31', open: 58.32, high: 58.32, low: 56.76, close: 57.09 },
-	{ time: '2018-11-01', open: 56.98, high: 57.28, low: 55.55, close: 56.05 },
-	{ time: '2018-11-02', open: 56.34, high: 57.08, low: 55.92, close: 56.63 },
-	{ time: '2018-11-05', open: 56.51, high: 57.45, low: 56.51, close: 57.21 },
-	{ time: '2018-11-06', open: 57.02, high: 57.35, low: 56.65, close: 57.21 },
-	{ time: '2018-11-07', open: 57.55, high: 57.78, low: 57.03, close: 57.65 },
-	{ time: '2018-11-08', open: 57.70, high: 58.44, low: 57.66, close: 58.27 },
-]
+var start = new Date(Date.now() - (7200 * 1000)).toISOString(); // 2 hours ago
+var symbol = "ETH/USD"
+var timeframe = "1Min"
+var bars_URL = `https://data.alpaca.markets/v1beta3/crypto/us/bars?symbols=${symbol}&timeframe=${timeframe}&start=${start}`
 
-candleSeries.setData(data)
+fetch(bars_URL, {
+    headers: {
+        'APCA-API-KEY-ID': API_KEY,
+        'APCA-API-SECRET-KEY': SECRET_KEY
+    }
+}).then((r) => r.json())
+    .then((response) => {
+        console.log(response);
+        
+        const data = response.bars[symbol].map(bar => (
+            {
+                open: bar.o,
+                high: bar.h,
+                low: bar.l,
+                close: bar.c,
+                time: Date.parse(bar.t) / 1000
+            }
+        ));
+
+        candleSeries.setData(data)
+
+
+
+    })
+
 
 webSocket.onmessage = (event) => {
     const data = JSON.parse(event.data)
@@ -76,8 +87,8 @@ webSocket.onmessage = (event) => {
         const type  = data[key].T
 
         if (type === 'q') {
-            console.log("Got a QUOTE")
-            console.log(data[key])
+            // console.log("Got a QUOTE")
+            // console.log(data[key])
 
             const quoteElement = document.createElement('div');
             quoteElement.className = 'quote'
@@ -90,8 +101,8 @@ webSocket.onmessage = (event) => {
             }
 
         } else if (type === 't') {
-            console.log("Got a TRADE")
-            console.log(data[key])
+            // console.log("Got a TRADE")
+            // console.log(data[key])
 
             const tradeElement = document.createElement('div');
             tradeElement.className = 'trade'
@@ -103,8 +114,8 @@ webSocket.onmessage = (event) => {
                 tradesElement.removeChild(elements[0])
             }
         } else if (type === 'b') {
-            console.log("Got a new BAR")
-            console.log(data[key])
+            // console.log("Got a new BAR")
+            // console.log(data[key])
         }
     }
 }
