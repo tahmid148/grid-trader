4ac1aa085b4832cea7653015ee5efd52761d3aac
Tahmid Rahman <tr920@ic.ac.uk>
2023-05-09 18:34:50 +0100

    feat: converted to use binance testnet and fetch ticker and place order

--- backend-python/config.py
+++ backend-python/config.py
@@ -13,4 +13,13 @@ def get_key_from_env():
     return api_key, secret_key
 
 API_KEY, SECRET_KEY = get_key_from_env()
-SYMBOL = os.getenv('SYMBOL')
+SYMBOL="ETHUSDT"
+POSITION_SIZE=0.001
+
+#GridBot Settings
+NUM_BUY_GRID_LINES = 5
+NUM_SELL_GRID_LINES = 5
+GRID_SIZE = 3
+
+CHECK_ORDERS_FREQUENCY = 2
+CLOSED_ORDER_STATUS = "closed"
--- backend-python/gridbot.py
+++ backend-python/gridbot.py
@@ -6,7 +6,16 @@ exchange = ccxt.binance({
     'secret': config.SECRET_KEY,
 })
 
+exchange.set_sandbox_mode(True)
+
 # Fetch current bid and ask prices
 ticker = exchange.fetch_ticker(config.SYMBOL)
-
 print(ticker)
+
+buy_orders = []
+sell_orders = []
+
+# Create market buy order
+initial_buy_order = exchange.create_market_buy_order(config.SYMBOL, 0.01)
+print("INITIAL BUY ORDER:")
+print(initial_buy_order)
