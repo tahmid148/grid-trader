76950678172867ef40402c0ddf67abd3f50b4021
Tahmid Rahman <tr920@ic.ac.uk>
2023-05-10 17:26:16 +0100

    feat: chart instance has been created with mock data

--- frontend/src/Chart.tsx
+++ frontend/src/Chart.tsx
@@ -1,4 +1,4 @@
-import { createChart } from "lightweight-charts";
+import { ColorType, CrosshairMode, createChart } from "lightweight-charts";
 import { useEffect, useRef } from "react";
 
 const Chart = ({ data }) => {
@@ -9,6 +9,31 @@ const Chart = ({ data }) => {
     const chart = createChart(chartContainerRef.current, {
       width: 600,
       height: 300,
+      layout: {
+        background: { type: ColorType.Solid, color: "#ffffff" },
+        textColor: "#000000",
+      },
+      rightPriceScale: {
+        borderColor: "#cccccc",
+      },
+      timeScale: {
+        borderColor: "#cccccc",
+        timeVisible: true,
+      },
+    });
+
+    chart.applyOptions({
+      grid: {
+        vertLines: {
+          color: "#ffffff",
+        },
+        horzLines: {
+          color: "#ffffff",
+        },
+      },
+      crosshair: {
+        mode: CrosshairMode.Normal,
+      },
     });
 
     // Create the main chart series
