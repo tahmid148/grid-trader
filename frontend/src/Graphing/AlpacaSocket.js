6de346eb44c9e50780ca06943c1a848a68538dc7
Tahmid Rahman <tr920@ic.ac.uk>
2023-05-11 00:18:02 +0100

    feat: introduced alpaca library to call api instead of using websocket

--- frontend/src/Graphing/AlpacaSocket.js
+++ frontend/src/Graphing/AlpacaSocket.js
@@ -1,3 +1,41 @@
+const Alpaca = require("@alpacahq/alpaca-trade-api");
+
+const API_KEY = "AKXXEXUBUX0WATW7WC28";
+const SECRET_KEY = "5mhAaEF7CwL26C4wCQ7Ww58pyVXfyyaiY35rwixI";
+
+const alpaca = new Alpaca({
+  keyId: API_KEY,
+  secretKey: SECRET_KEY,
+  paper: true,
+});
+
+const options = {
+  start: new Date(new Date().setDate(new Date().getDate() - 1)), // 1 day ago
+  end: new Date(), // Current date
+  timeframe: "1Day",
+};
+
+async function getHistoricalBars(symbols) {
+  let bars = [];
+  try {
+    const resp = await alpaca.getCryptoBars(symbols, options);
+    for await (let bar of resp) {
+      bars.push(bar);
+    }
+  } catch (error) {
+    console.error("Error fetching historical bars:", error);
+  }
+  return bars;
+}
+
+const symbols = ["ETH/USD"]; // Pass symbols as an array
+const barsPromise = getHistoricalBars(symbols);
+barsPromise.then((bars) =>
+  bars.forEach((bar) => {
+    console.log(bar);
+  })
+);
+
 const sendAuthentication = (webSocket, API_KEY, SECRET_KEY) => {
   const authMessage = { action: "auth", key: API_KEY, secret: SECRET_KEY };
   webSocket.send(JSON.stringify(authMessage));
@@ -13,4 +51,4 @@ const sendSubscription = (webSocket) => {
   webSocket.send(JSON.stringify(subscribeMessage));
 };
 
-export { sendAuthentication, sendSubscription };
+// export { sendAuthentication, sendSubscription };
--- frontend/src/Graphing/Chart.tsx
+++ frontend/src/Graphing/Chart.tsx
@@ -1,13 +1,11 @@
 import { ColorType, CrosshairMode, createChart } from "lightweight-charts";
 import { useEffect, useRef } from "react";
-import { sendAuthentication, sendSubscription } from "./AlpacaSocket";
+// import Alpaca from "@alpacahq/alpaca-trade-api";
 
 const Chart = () => {
   const chartContainerRef = useRef(null);
-  const API_KEY = "AKXXEXUBUX0WATW7WC28";
-  const SECRET_KEY = "5mhAaEF7CwL26C4wCQ7Ww58pyVXfyyaiY35rwixI";
-  const WEBSOCKET_URL = "wss://stream.data.alpaca.markets/v1beta3/crypto/us";
-  let initialised = false; // Used to prevent useEffect from running multiple times
+  const API_KEY = process.env.API_KEY;
+  const SECRET_KEY = process.env.SECRET_KEY;
 
   const data = [
     { time: "2018-10-19", open: 54.62, high: 55.5, low: 54.52, close: 54.9 },
@@ -55,56 +53,6 @@ const Chart = () => {
 
     // Create the main chart series
     const candleSeries = chart.addCandlestickSeries();
-
-    if (!initialised) {
-      initialised = true;
-
-      const webSocket = new WebSocket(WEBSOCKET_URL);
-
-      webSocket.onmessage = (event) => {
-        const data = JSON.parse(event.data);
-        const message = data[0]["msg"];
-
-        if (message === "connected") {
-          console.log("Requesting Authentication");
-          sendAuthentication(webSocket, API_KEY, SECRET_KEY);
-        } else if (message === "authenticated") {
-          console.log("Authentication Successful!");
-          sendSubscription(webSocket);
-        } else if (message === "auth failed") {
-          console.log("Authentication Failed");
-        } else if (message === "auth timeout") {
-          console.log("Authentication Timed Out");
-        }
-      };
-
-      var start = new Date(Date.now() - 7200 * 1000).toISOString(); // 2 hours ago
-      var symbol = "ETH/USD";
-      var timeframe = "1Min";
-      var bars_URL = `https://data.alpaca.markets/v1beta3/crypto/us/bars?symbols=${symbol}&timeframe=${timeframe}&start=${start}`;
-
-      fetch(bars_URL, {
-        headers: {
-          "APCA-API-KEY-ID": API_KEY,
-          "APCA-API-SECRET-KEY": SECRET_KEY,
-        },
-      })
-        .then((r) => r.json())
-        .then((response) => {
-          console.log(response);
-
-          const data = response.bars[symbol].map((bar) => ({
-            open: bar.o,
-            high: bar.h,
-            low: bar.l,
-            close: bar.c,
-            time: Date.parse(bar.t) / 1000,
-          }));
-
-          candleSeries.setData(data);
-          // currentBar = data[data.length - 1];
-        });
-    }
 
     // Add the data to the chart
     candleSeries.setData(data);
