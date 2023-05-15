6e23fb8a195406015f93d446105784aa3013cc56
Tahmid Rahman <tr920@ic.ac.uk>
2023-05-15 17:47:10 +0100

    feat: cleaned up client and server websocket implementation

--- backend-python/gridbot_websocket_client.py
+++ backend-python/gridbot_websocket_client.py
@@ -10,80 +10,83 @@ exchange.set_sandbox_mode(True)
 
 # Fetch current bid and ask prices
 ticker = exchange.fetch_ticker(config.SYMBOL)
-print(ticker)
+# print(ticker)
 
 buy_orders = []
 sell_orders = []
 
-# Create market buy order
+# Print initial buy order
 # initial_buy_order = exchange.create_market_buy_order(config.SYMBOL, 0.01)
+# print(initial_buy_order)
 
 for i in range(config.NUM_BUY_GRID_LINES):
     initial_price = ticker['bid']
-    order_price = initial_price - (i * config.GRID_SIZE) # Order should be the below and dependant on size of the grid
+    order_price = initial_price - ((i + 1) * config.GRID_SIZE) # Order should be the below and dependant on size of the grid
     print("Submitting market limit buy order for " + str(config.POSITION_SIZE) + " at " + str(order_price))
     order = exchange.create_limit_buy_order(config.SYMBOL, config.POSITION_SIZE, order_price)
     buy_orders.append(order['info'])
 
 for i in range(config.NUM_SELL_GRID_LINES):
-    initial_price = ticker['bid'] + (i * config.GRID_SIZE)
-    order_price = initial_price + (i * config.GRID_SIZE) # Order should be the above and dependant on size of the grid
+    initial_price = ticker['bid']
+    order_price = initial_price + ((i + 1) * config.GRID_SIZE) # Order should be the above and dependant on size of the grid
     print("Submitting market limit sell order for " + str(config.POSITION_SIZE) + " at " + str(order_price))
     order = exchange.create_limit_sell_order(config.SYMBOL, config.POSITION_SIZE, order_price)
     sell_orders.append(order['info'])
 
+
 while True:
     closed_order_ids = []
+
     for buy_order in buy_orders:
-        print("Checking buy orders for " + str(buy_order['orderId']))
+        print("Checking Buy Order " + str(buy_order['orderId']))
         try:
             order = exchange.fetch_order(buy_order['orderId'], config.SYMBOL)
-            # print(order)
         except Exception as e:
             print("Request Failed, Retrying...")
             continue
-
+        
         order_info = order['info']
+        order_status = order_info['status']
 
-        if order_info == config.CLOSED_ORDER_STATUS:
-            # Remove the closed Buy Order
+        if order_status == config.CLOSED_ORDER_STATUS:
             closed_order_ids.append(order_info['orderId'])
             print("Buy Order Executed at " + str(order_info['price']))
-            # Create Sell Order
-            new_sell_price = order_info['price'] + config.GRID_SIZE
-            print("Creating New Limit Sell Order at " + str(new_sell_price)) 
+            # Create Limit Sell Order
+            new_sell_price = float(order_info['price']) + config.GRID_SIZE
+            print("Creating New Limit Sell Order at " + str(new_sell_price))
             new_sell_order = exchange.create_limit_sell_order(config.SYMBOL, config.POSITION_SIZE, new_sell_price)
             sell_orders.append(new_sell_order)
-             
+
         time.sleep(config.CHECK_ORDERS_FREQUENCY)
 
-    print("Checking for open sell orders:")
     for sell_order in sell_orders:
-        print("Checking Sell Orders for " + str(sell_order['orderId']))
+        print("Checking Sell Order " + str(sell_order['orderId']))
         try:
             order = exchange.fetch_order(sell_order['orderId'], config.SYMBOL)
-            # print(order)
         except Exception as e:
             print("Request Failed, Retrying...")
             continue
 
         order_info = order['info']
+        order_status = order_info['status']
 
-        if order_info == config.CLOSED_ORDER_STATUS:
-            # Remove the closed sell Order
+        if order_status == config.CLOSED_ORDER_STATUS:
             closed_order_ids.append(order_info['orderId'])
-            print("Buy Sell Executed at " + str(order_info['price']))
-            # Create Buy Order
-            new_buy_price = order_info['price'] - config.GRID_SIZE
-            print("Creating New Limit Sell Order at " + str(new_buy_price)) 
+            print("Sell Order Executed at " + str(order_info['price']))
+            # Create Limit Buy Order
+            new_buy_price = float(order_info['price']) - config.GRID_SIZE
+            print("Creating New Limit Buy Order at " + str(new_buy_price))
             new_buy_order = exchange.create_limit_buy_order(config.SYMBOL, config.POSITION_SIZE, new_buy_price)
             buy_orders.append(new_buy_order)
-             
+
         time.sleep(config.CHECK_ORDERS_FREQUENCY)
 
-    for order_id in closed_order_ids:
-        buy_orders = [buy_order for buy_order in buy_orders if buy_order['orderId'] != order_id]
-        sell_orders = [sell_order for sell_order in sell_orders if sell_order['orderId'] != order_id]
-    
-    if (sell_orders ==0):
-        sys.exit("Stopping bot, nothing left to sell")
+    # Remove closed orders from buy_orders and sell_orders
+    for closed_order_id in closed_order_ids:
+        buy_orders = [buy_order for buy_order in buy_orders if buy_order['orderId'] != closed_order_id]
+        sell_orders = [sell_order for sell_order in sell_orders if sell_order['orderId'] != closed_order_id]
+
+    # Stop the bot if there are no more open sell orders
+    if len(sell_orders) == 0:
+        print("All sell orders have been closed, stopping bot!")
+        sys.exit() 
--- backend-python/gridbot_websocket_server.py
+++ backend-python/gridbot_websocket_server.py
@@ -2,22 +2,17 @@ from websocket_server import WebsocketServer
 
 # Called for every client connecting (after handshake)
 def new_client(client, server):
-    print("New client connected and was given id %d" % client['id'])
-    server.send_message_to_all("Hey all, a new client has joined us")
-
-# Called for every client disconnecting
-def client_left(client, server):
-    print("Client(%d) disconnected" % client['id'])
-    server.send_message_to_all("Hey all, a client has left us")
+	print("New client connected and was given id %d" % client['id'])
+	server.send_message_to_all("Hey all, a new client has joined us")
 
 # Called when a client sends a message
 def message_received(client, server, message):
     print("Client(%d) said: %s" % (client['id'], message))
-    server.send_message(client, "You said: " + message)
+    server.send_message_to_all(message)
 
-PORT=9001
-server = WebsocketServer(port=PORT)
+
+PORT = 9001
+server = WebsocketServer(port = PORT)
 server.set_fn_new_client(new_client)
-server.set_fn_client_left(client_left)
 server.set_fn_message_received(message_received)
 server.run_forever()
