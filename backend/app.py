a667b731900fecfa621fb4125e4fa4871f356585
Tahmid Rahman <tr920@ic.ac.uk>
2023-05-16 14:55:46 +0100

    feat: FastAPI Websocket set up to broadcast information

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,22 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from typing import List
+import connection_manager
 
 app = FastAPI()
 
-connected_clients: List[WebSocket] = []
+manager = connection_manager.ConnectionManager()
 
 
-@app.websocket("/ws")
+@app.websocket("/")
 async def websocket_endpoint(websocket: WebSocket):
-    # Accept the WebSocket connection
-    await websocket.accept()
-
-    # Add the WebSocket to the list of connected clients
-    connected_clients.append(websocket)
-
+    await manager.connect(websocket)
     try:
         while True:
-            # Wait for a message from the client
             data = await websocket.receive_text()
-
-            # Send the message to all connected clients
-            for (index, client) in enumerate(connected_clients):
-                await client.send_text(f"Client {index} says: {data}")
-    except Exception as e:
-        print(f"WebSocket connection error: {e}")
+            await manager.broadcast(data)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        print("A client has left the chat")
 
 
 if __name__ == "__main__":
