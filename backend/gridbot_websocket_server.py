ea5c7444d2ec53a6f75bc8428d3025a0e56fe72b
Tahmid Rahman <tr920@ic.ac.uk>
2023-05-23 16:52:21 +0100

    feat: websocket server reconfigured to recieve a payload to know which clients are backend and frontend

--- backend/gridbot_websocket_server.py
+++ backend/gridbot_websocket_server.py
@@ -1,17 +1,48 @@
+import json
 from websocket_server import WebsocketServer
 
-# Called for every client connecting (after handshake)
+backend_clients = []
+frontend_clients = []
+
+
+def assign_client_type(client, message_json):
+    if message_json['client_type'] == 'backend':
+        backend_clients.append(client)
+        print("Backend client connected")
+    elif message_json['client_type'] == 'frontend':
+        frontend_clients.append(client)
+        print("Frontend client connected")
+
+
+def send_message_to_frontend(message):
+    print("Sending message to frontend")
+    for client in frontend_clients:
+        server.send_message(client, json.dumps(message))
+
+
+def send_message_to_backend(message):
+    print("Sending message to backend")
+    for client in backend_clients:
+        server.send_message(client, json.dumps(message))
 
 
 def new_client(client, server):
     print("New client connected and was given id %d" % client['id'])
 
-# Called when a client sends a message
-
 
 def message_received(client, server, message):
-    print("Client(%d) said: %s" % (client['id'], message))
-    server.send_message_to_all(message)
+    try:
+        message_json = json.loads(message)
+        if "client_type" in message_json:
+            assign_client_type(client, message_json)
+        elif client in frontend_clients:
+            send_message_to_backend(message_json)
+        elif client in backend_clients:
+            send_message_to_frontend(message_json)
+    except json.decoder.JSONDecodeError:
+        print("Message not JSON")
+
+    # print("Client(%d) said: %s" % (client['id'], message))
 
 
 PORT = 9001
