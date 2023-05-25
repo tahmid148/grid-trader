85444f7df2860be4abcb05444fd81ad0654c262e
Tahmid Rahman <tr920@ic.ac.uk>
2023-05-25 23:03:42 +0100

    feat: closed orders now shows modals on click

--- frontend/src/Dashboard/ClosedOrdersTable.tsx
+++ frontend/src/Dashboard/ClosedOrdersTable.tsx
@@ -1,6 +1,18 @@
-import { Table } from "react-bootstrap";
+import { useState } from "react";
+import { Modal, Table } from "react-bootstrap";
 
 const ClosedOrdersTable = ({ closedOrders }) => {
+  const [selectedEntry, setSelectedEntry] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleTableRowClick = (entry) => {
+    setSelectedEntry(entry);
+    setShowModal(true);
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
   return (
     <div className="scrollable-table">
       <h2>Closed Orders</h2>
@@ -15,7 +27,7 @@ const ClosedOrdersTable = ({ closedOrders }) => {
         <tbody>
           {closedOrders.map((quote, index) => {
             return (
-              <tr key={index}>
+              <tr key={index} onClick={() => handleTableRowClick(quote)}>
                 <td>{quote.side}</td>
                 <td>{quote.price}</td>
                 <td>{quote.origQty}</td>
@@ -24,6 +36,19 @@ const ClosedOrdersTable = ({ closedOrders }) => {
           })}
         </tbody>
       </Table>
+      <Modal show={showModal} onHide={handleModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Order Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Side: {selectedEntry?.side}</p>
+          <p>Price: {selectedEntry?.price}</p>
+          <p>Quantity: {selectedEntry?.origQty}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <button onClick={handleModalClose}>Close</button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
