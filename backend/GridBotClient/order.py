5e9f35502887da602aea8c44aee7eb9851f44ef5
Tahmid Rahman <tr920@ic.ac.uk>
2023-06-07 13:18:33 +0100

    feat: updated order object to now send whether the sell or buy order is open

--- backend/GridBotClient/order.py
+++ backend/GridBotClient/order.py
@@ -42,85 +42,10 @@ class Order:
             return {
                 "buy_order": self.buy_order,
                 "sell_order": self.sell_order,
-                "open": not self.is_closed(),
+                "open_buy": not self.is_buy_order_closed(),
+                "open_sell": not self.is_sell_order_closed(),
             }
         elif self.buy_order:
-            return {"buy_order": self.buy_order, "sell_order": None, "open": True}
+            return {"buy_order": self.buy_order, "sell_order": None, "open_buy": not self.is_buy_order_closed(), "open_sell": True}
         elif self.sell_order:
-            return {"sell_order": self.sell_order, "buy_order": None, "open": True}
-
-
-x = {
-    "info": {
-        "symbol": "ETHUSDT",
-        "orderId": "12280707",
-        "orderListId": "-1",
-        "clientOrderId": "x-R4BD3S824f8e87255a9b53554ab25a",
-        "transactTime": "1685985441683",
-        "price": "0.00000000",
-        "origQty": "0.05000000",
-        "executedQty": "0.05000000",
-        "cummulativeQuoteQty": "90.30450000",
-        "status": "FILLED",
-        "timeInForce": "GTC",
-        "type": "MARKET",
-        "side": "BUY",
-        "workingTime": "1685985441683",
-        "fills": [
-            {
-                "price": "1806.09000000",
-                "qty": "0.05000000",
-                "commission": "0.00000000",
-                "commissionAsset": "ETH",
-                "tradeId": "2128279",
-            }
-        ],
-        "selfTradePreventionMode": "NONE",
-    },
-    "id": "12280707",
-    "clientOrderId": "x-R4BD3S824f8e87255a9b53554ab25a",
-    "timestamp": 1685985441683,
-    "datetime": "2023-06-05T17:17:21.683Z",
-    "lastTradeTimestamp": 1685985441683,
-    "symbol": "ETH/USDT",
-    "type": "market",
-    "timeInForce": "GTC",
-    "postOnly": False,
-    "reduceOnly": None,
-    "side": "buy",
-    "price": 1806.09,
-    "triggerPrice": None,
-    "amount": 0.05,
-    "cost": 90.3045,
-    "average": 1806.09,
-    "filled": 0.05,
-    "remaining": 0.0,
-    "status": "closed",
-    "fee": {"currency": None, "cost": None, "rate": None},
-    "trades": [
-        {
-            "info": {
-                "price": "1806.09000000",
-                "qty": "0.05000000",
-                "commission": "0.00000000",
-                "commissionAsset": "ETH",
-                "tradeId": "2128279",
-            },
-            "timestamp": None,
-            "datetime": None,
-            "symbol": "ETH/USDT",
-            "id": "2128279",
-            "order": None,
-            "type": None,
-            "side": None,
-            "takerOrMaker": None,
-            "price": 1806.09,
-            "amount": 0.05,
-            "cost": 90.3045,
-            "fee": {"cost": 0.0, "currency": "ETH"},
-            "fees": [{"cost": 0.0, "currency": "ETH"}],
-        }
-    ],
-    "fees": [{"currency": None, "cost": None, "rate": None}],
-    "stopPrice": None,
-}
+            return {"sell_order": self.sell_order, "buy_order": None, "open_buy": True, "open_sell": not self.is_sell_order_closed()}
