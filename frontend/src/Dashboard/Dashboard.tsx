1ec2a93e07c8cd225d980c4733ed713d2fe3baa2
Tahmid Rahman <tr920@ic.ac.uk>
2023-06-08 19:08:26 +0100

    feat: changed how start date is declared

--- frontend/src/Dashboard/Dashboard.tsx
+++ frontend/src/Dashboard/Dashboard.tsx
@@ -3,10 +3,11 @@ import Chart from "./Chart";
 import { useEffect, useState } from "react";
 
 const getBars = async () => {
-  const start = new Date(Date.now() - 7200 * 1000).toISOString(); // 2 hours ago
+  const start = new Date();
+  start.setHours(start.getHours() - 3); // 1 hour ago
   const symbol = "ETH/USDT";
   const timeframe = "1Min";
-  const bars_URL = `https://data.alpaca.markets/v1beta3/crypto/us/bars?symbols=${symbol}&timeframe=${timeframe}&start=${start}`;
+  const bars_URL = `https://data.alpaca.markets/v1beta3/crypto/us/bars?symbols=${symbol}&timeframe=${timeframe}&start=${start.toISOString()}`;
 
   const response = await axios.get(bars_URL, {
     headers: {
