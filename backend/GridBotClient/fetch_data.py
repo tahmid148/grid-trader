1a6d52c25ab4f15c8153d3c9f74d9452518432c1
Tahmid Rahman <tr920@ic.ac.uk>
2023-06-13 14:36:43 +0100

    feat: command needs to backtest

--- backend/GridBotClient/fetch_data.py
+++ backend/GridBotClient/fetch_data.py
@@ -19,19 +19,22 @@ symbol = 'ETH/USDT'
 timeframe = '1m'
 
 # Define the desired time range
-start_date = datetime.datetime(2022, 1, 1)  # Specify the start date
-end_date = datetime.datetime(2022, 1, 5)  # Specify the end date
+start_date = datetime.datetime(2023, 6, 8)  # Specify the start date
+# Specify the end date (2 days later)
+end_date = start_date + datetime.timedelta(days=2)
 
 # Convert the datetime objects to timestamps
 start_timestamp = int(start_date.timestamp() * 1000)
 end_timestamp = int(end_date.timestamp() * 1000)
+
+print(f"Start date: {start_date}: {start_timestamp}")
+print(f"End date: {end_date}: {end_timestamp}")
 
 # Set the maximum number of candles to fetch per API call
 limit = 1000
 
 # Fetch OHLCV data for the specified time range
 ohlcv_data = []
-
 current_timestamp = start_timestamp
 
 while current_timestamp <= end_timestamp:
@@ -50,7 +53,7 @@ while current_timestamp <= end_timestamp:
 for candle in ohlcv_data:
     timestamp, open_, high, low, close, volume = candle
     print(
-        f"Timestamp: {timestamp}, Open: {open_}, High: {high}, Low: {low}, Close: {close}, Volume: {volume}")
+        f"Timestamp: {datetime.datetime.fromtimestamp(timestamp / 1000).strftime('%Y-%m-%d %H:%M:%S')}, Open: {open_}, High: {high}, Low: {low}, Close: {close}, Volume: {volume}")
 
 # Define the path of the CSV file
 csv_file = 'BacktestingClient/data_directory/ohlcv/binance/ETHUSDT/ETHUSDT.csv'
@@ -59,7 +62,6 @@ headers = ["", "open", "high", "low", "close", "volume"]
 # Convert the OHLCV data to the desired timestamp format
 formatted_ohlcv_data = [[datetime.datetime.fromtimestamp(
     candle[0] / 1000).strftime('%Y-%m-%d %H:%M:%S'), *candle[1:]] for candle in ohlcv_data]
-
 
 # Write the OHLCV data to the CSV file
 with open(csv_file, 'w', newline='') as file:
