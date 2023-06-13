9d861a0df45de74ea7ea293bf25a23ee2e5d915e
Tahmid Rahman <tr920@ic.ac.uk>
2023-06-13 13:27:59 +0100

    feat: introduced python file that can fetch historical data

---
+++ backend/GridBotClient/fetch_data.py
@@ -0,0 +1,52 @@
+import ccxt
+import config
+import datetime
+
+# Connect to exchange
+exchange = ccxt.binance(
+    {
+        "apiKey": config.API_KEY,
+        "secret": config.SECRET_KEY,
+    }
+)
+
+exchange.set_sandbox_mode(True)
+print("Connected to exchange")
+
+# Define the trading pair and timeframe
+symbol = 'ETH/USDT'
+timeframe = '1m'
+
+# Define the desired time range
+start_date = datetime.datetime(2022, 1, 1)  # Specify the start date
+end_date = datetime.datetime(2022, 1, 5)  # Specify the end date
+
+# Convert the datetime objects to timestamps
+start_timestamp = int(start_date.timestamp() * 1000)
+end_timestamp = int(end_date.timestamp() * 1000)
+
+# Set the maximum number of candles to fetch per API call
+limit = 1000
+
+# Fetch OHLCV data for the specified time range
+ohlcv_data = []
+
+current_timestamp = start_timestamp
+
+while current_timestamp <= end_timestamp:
+    # Fetch OHLCV data for the current chunk
+    chunk_data = exchange.fetch_ohlcv(
+        symbol, timeframe, since=current_timestamp, limit=limit)
+
+    # Append the chunk data to the OHLCV data list
+    ohlcv_data.extend(chunk_data)
+
+    # Update the current timestamp to the next chunk
+    # Set the next timestamp after the last candle
+    current_timestamp = chunk_data[-1][0] + 1
+
+# Print the fetched OHLCV data
+for candle in ohlcv_data:
+    timestamp, open_, high, low, close, volume = candle
+    print(
+        f"Timestamp: {timestamp}, Open: {open_}, High: {high}, Low: {low}, Close: {close}, Volume: {volume}")
