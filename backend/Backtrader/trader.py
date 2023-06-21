5c7ecbe0bb8eaa52f2b2e762ff53f8561690b517
Tahmid Rahman <tr920@ic.ac.uk>
2023-06-21 21:37:34 +0100

    feat: time fix

--- backend/Backtrader/trader.py
+++ backend/Backtrader/trader.py
@@ -1,10 +1,31 @@
 import backtrader
+import datetime
 
 cerebro = backtrader.Cerebro()
 
 cerebro.broker.setcash(1000000)
 
 print(f"Starting Portfolio Value: {cerebro.broker.getvalue()}")
+
+data = backtrader.GenericCSVData(
+    dataname='./Backtrader/ETHUSDT.csv',
+
+    fromdate=datetime.datetime(2000, 1, 1),
+    todate=datetime.datetime(2000, 12, 31),
+
+    nullvalue=0.0,
+
+    dtformat=('%Y-%m-%d'),
+
+    datetime=0,
+    high=1,
+    low=2,
+    open=3,
+    close=4,
+    volume=5,
+    openinterest=-1
+)
+
 
 cerebro.run()
 
