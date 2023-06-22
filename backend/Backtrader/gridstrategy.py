3f9026f7546876e842578c79aed6fb453c6440ef
Tahmid Rahman <tr920@ic.ac.uk>
2023-06-22 16:46:47 +0100

    starting over

--- backend/Backtrader/gridstrategy.py
+++ backend/Backtrader/gridstrategy.py
@@ -3,53 +3,15 @@ import backtrader
 
 class GridStrategy(backtrader.Strategy):
 
-    # Used to print out information on orders
     def log(self, txt, dt=None):
         ''' Logging function for this strategy'''
-        dt = dt or self.datas[0].datetime.datetime(0)
-        print(f'{dt.isoformat()}, {txt}')
+        dt = dt or self.datas[0].datetime.date(0)
+        print(f"{dt.isoformat()}, {txt}")
 
     def __init__(self):
-        # Define parameters for grid trading strategy
-        self.position_size = 0.1
-        self.grid_size = 1
-        self.no_of_grids = 5
-
         # Keep a reference to the "close" line in the data[0] dataseries
         self.dataclose = self.datas[0].close
 
-        # To keep track of pending orders
-        self.order = None
-
-    def notify_order(self, order):
-        if order.status in [order.Submitted, order.Accepted]:
-            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
-            return
-
-        # Check if an order has been completed
-        # Attention: broker could reject order if not enough cash
-        if order.status in [order.Completed]:
-            if order.isbuy():
-                self.log('BUY EXECUTED, %.2f' % order.executed.price)
-            elif order.issell():
-                self.log('SELL EXECUTED, %.2f' % order.executed.price)
-
-            self.bar_executed = len(self)
-
-        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
-            self.log('Order Canceled/Margin/Rejected')
-
-        # Write down: no pending order
-        self.order = None
-
     def next(self):
         # Simply log the closing price of the series from the reference
-        self.log(f'Close, {self.dataclose[0]}', )
-
-        if self.order == None:
-            # If there are no orders, create a new order
-            # BUY, BUY, BUY!!! (with default parameters)
-            self.log('BUY CREATE, %.2f' % self.dataclose[0])
-
-            buy_order = self.buy()
-            self.order = buy_order
+        self.log(f"Close, {self.dataclose[0]}")
--- backend/Backtrader/trader.py
+++ backend/Backtrader/trader.py
@@ -7,13 +7,26 @@ cerebro = backtrader.Cerebro()
 cerebro.broker.setcash(1000000)
 
 # Create a Data Feed
-data = backtrader.feeds.YahooFinanceCSVData(
+data = backtrader.feeds.GenericCSVData(
     dataname='./Backtrader/ETHUSDT.csv',
+
     # Do not pass values before this date
     fromdate=datetime.datetime(2023, 6, 1, 0, 0, 0),
-    # Do not pass values after this date
     todate=datetime.datetime(2023, 6, 2, 23, 59, 0),
-    reverse=False)
+
+    # Value for missing data
+    nullvalue=0.0,
+
+    # Date time format
+    dtformat=('%Y-%m-%d %H:%M:%S'),
+
+    datetime=0,
+    open=1,
+    high=2,
+    low=3,
+    close=4,
+    openinterest=-1
+)
 
 cerebro.adddata(data)
 
